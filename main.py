6f031a6ed4c285c8b7fce9d81b76c8f71a8208f2
Daniel Greeninger <daniel.greeninger@hashicorp.com>
2022-06-22 15:48:31 -0700

    launch a GKE cluster

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack, RemoteBackend, NamedRemoteWorkspace, TerraformOutput, TerraformAsset, AssetType
+
+from cdktf_cdktf_provider_google import (
+    CloudfunctionsFunctionIamBinding,
+    GoogleProvider,
+    CloudfunctionsFunction,
+    StorageBucket,
+    StorageBucketObject,
+    ContainerCluster,
+    ContainerNodePool,
+    ContainerRegistry,
+    DataGoogleContainerCluster,
+    ProjectIamMember,
+    ServiceAccount
+)
+
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+
+        # define resources here
+        GoogleProvider(self, id=project_id, region="us-central1", zone="us-central1-a", project=project_id)
+        cluster = ContainerCluster(self, "cluster", \
+                name="rubrik-test-cluster",
+                initial_node_count=2,
+            )
+
+
+
+
+app = App()
+project_id = "hc-8255d8aec51f4af78c98df80971"
+stack = MyStack(app, "gke")
+RemoteBackend(stack,
+  hostname='app.terraform.io',
+  organization='dgreeninger',
+  workspaces=NamedRemoteWorkspace('gke')
+)
+
+app.synth()
